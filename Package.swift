7873a55e01a020d7bcad0893ad1da68aa627f004
Paul Beusterien <paulbeusterien@google.com>
2021-01-29 10:02:20 -0800

    GU 7.2.1

--- Package.swift
+++ Package.swift
@@ -30,8 +30,7 @@ let package = Package(
     .package(
       name: "GoogleUtilities",
       url: "https://github.com/google/GoogleUtilities.git",
-      .exact("7.2.1-test")
-//      "7.2.0" ..< "8.0.0"
+      "7.2.1" ..< "8.0.0"
     ),
     .package(
       name: "nanopb",
